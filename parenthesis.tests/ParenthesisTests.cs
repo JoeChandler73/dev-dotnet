f081b5534263ae970fbd0a1b9e0677c203b35dff
Joe Chandler <joseph-chandler@sky.com>
2026-02-02 15:07:11 +0000

    Initial Commit

---
+++ parenthesis.tests/ParenthesisTests.cs
@@ -0,0 +1,39 @@
+﻿namespace parenthesis.tests;
+
+public class ParenthesisTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var is_valid = Parenthesis.IsValid("()");
+        Assert.True(is_valid);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var is_valid = Parenthesis.IsValid("()[]{}");
+        Assert.True(is_valid);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var is_valid = Parenthesis.IsValid("(]");
+        Assert.False(is_valid);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var is_valid = Parenthesis.IsValid("([])");
+        Assert.True(is_valid);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var is_valid = Parenthesis.IsValid("([)]");
+        Assert.False(is_valid);
+    }
+}
---
+++ parenthesis/Parenthesis.cs
@@ -0,0 +1,35 @@
+﻿namespace parenthesis;
+
+public static class Parenthesis
+{
+    public static bool IsValid(string s)
+    {
+        if (s.Length % 2 != 0)
+            return false;
+
+        var stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                var token = stack.Pop();
+
+                if (token == '(' && c != ')')
+                    return false;
+                if (token == '[' && c != ']')
+                    return false;
+                if (token == '{' && c != '}')
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
